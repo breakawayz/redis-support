9e8364c14d844ba6b5ed48c1488d2deba46b079a
breakawayz <375831500@qq.com>
2017-09-23 23:09:40 +0800

    Update JedisClusterFactoryBean.java

--- jedis-spring-support/src/main/java/com/zhangyx/hw/support/JedisClusterFactoryBean.java
+++ jedis-spring-support/src/main/java/com/zhangyx/hw/support/JedisClusterFactoryBean.java
@@ -1,8 +1,6 @@
 package com.zhangyx.hw.support;
 
 /**
- * 通过cms系统配置redis，并接入监控系统
- * Created by lirui on 15/1/18.
  */
 public class JedisClusterFactoryBean extends JedisClusterFactory<MergeJedisCmd> {
 }
